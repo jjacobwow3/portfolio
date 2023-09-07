a98caace0bdd0c1665853ad5804a54cbba89f166
Bleedem <jjacobwow@gmail.com>
2023-09-07 14:57:17 -0400

    fade-ins

---
+++ app.js
@@ -0,0 +1,31 @@
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }       
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+let options = {
+    threshold: 0.1,
+};
+
+const proj = document.getElementsByClassName("project-container");
+const x = document.getElementsByClassName("intro");
+
+
+let projectObserver = new IntersectionObserver(callback, options);
+
+projectObserver.observe(proj);
+
+let callback = (entries, projectObserver) => {
+    entries.forEach((entry) => {
+        if (projectObserver.observe(entry)) {
+            entry.target.classList.add('show');
+        };
+    });
+};
