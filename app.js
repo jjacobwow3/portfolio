474502d29d4763c9c933c53d8a1683e4aa4112e2
Bleedem <jjacobwow@gmail.com>
2023-09-07 17:38:45 -0400

    fixed fade in
    goes earlier

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const observerOptions = {
-    root: null,
-    threshold: 0,
-    rootMargin: '0 0 -50px 0'
-};
-
 const observer = new IntersectionObserver((entries)=> {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,20 +5,17 @@ const observer = new IntersectionObserver((entries)=> {
             entry.target.classList.add('show');
         }       
     });
-}, observerOptions);
+});
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// Project fade in
 /*
 let options = {
     threshold: 0.1,
 };
 
 const proj = document.getElementsByClassName("project-container");
-const x = document.getElementsByClassName("intro");
-
 
 let projectObserver = new IntersectionObserver(callback, options);
 
@@ -37,4 +28,4 @@ let callback = (entries, projectObserver) => {
         };
     });
 };
-/*
+*/
